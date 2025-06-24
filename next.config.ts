f9eaa00f055ce5676482866089eddd0b52f32d73
Cr Space <crspace.vs@gmail.com>
2025-06-24 21:58:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'th.bing.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
