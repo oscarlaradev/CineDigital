aa1a9850947691c397c8aa65f35319be176bf9fb
Cr Space <crspace.vs@gmail.com>
2025-06-24 22:47:05 +0000

    no pero osea si quiero que se vea animado el texto , pero lo que quiero

--- src/components/animated-logo.tsx
+++ src/components/animated-logo.tsx
@@ -5,7 +5,16 @@ const AnimatedLogo = () => {
     <a href="#" className="animated-logo-container">
       <svg viewBox="0 0 800 150">
         <text
-          className="animated-logo-text"
+          className="animated-logo-text-base"
+          x="50%"
+          y="50%"
+          dy=".35em"
+          textAnchor="middle"
+        >
+          Cine Digital
+        </text>
+        <text
+          className="animated-logo-text-highlight"
           x="50%"
           y="50%"
           dy=".35em"
