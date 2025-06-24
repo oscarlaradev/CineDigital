7040a23a46a80fdf6060e2fc30608913eb870a9a
Cr Space <crspace.vs@gmail.com>
2025-06-24 22:44:56 +0000

    necesito que la ainimacion este mas mejor mejorala porque por la animaci

--- src/components/animated-logo.tsx
+++ src/components/animated-logo.tsx
@@ -3,33 +3,16 @@ import React from 'react';
 const AnimatedLogo = () => {
   return (
     <a href="#" className="animated-logo-container">
-      <svg viewBox="0 0 800 600">
-        <symbol id="s-text">
-          <text
-            textAnchor="middle"
-            x="50%"
-            y="40%"
-            className="text--line"
-          >
-            Cine
-          </text>
-          <text
-            textAnchor="middle"
-            x="50%"
-            y="80%"
-            className="text--line"
-          >
-            Digital
-          </text>
-        </symbol>
-
-        <g className="g-ants">
-          <use href="#s-text" className="text-copy"></use>
-          <use href="#s-text" className="text-copy"></use>
-          <use href="#s-text" className="text-copy"></use>
-          <use href="#s-text" className="text-copy"></use>
-          <use href="#s-text" className="text-copy"></use>
-        </g>
+      <svg viewBox="0 0 800 150">
+        <text
+          className="animated-logo-text"
+          x="50%"
+          y="50%"
+          dy=".35em"
+          textAnchor="middle"
+        >
+          Cine Digital
+        </text>
       </svg>
     </a>
   );
